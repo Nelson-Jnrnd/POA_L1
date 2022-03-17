0d965208c1b57172cb55893285381bae8b3a6d2f
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-03-17 08:47:42 +0100

    main arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
+#include <sstream>
 #include "Matrix.h"
 using std::cout;
 using std::endl;
 
-int main() {
+void testMatrix(unsigned n1, unsigned m1, unsigned n2, unsigned m2, unsigned n){
     cout << "Test initialisation" << endl;
 
-    Matrix* m1 = new Matrix(3, 3, 10);
+    Matrix* matrix1 = new Matrix(n1, m1, n);
 
-    Matrix* m2 = new Matrix(3, 3, 10);
+    Matrix* matrix2 = new Matrix(n2, m2, n);
 
-    Matrix* m3 = new Matrix(5,5,10);
+    Matrix* matrix3 = new Matrix(n1+1, m1+1, n);
 
-    Matrix* m4 = new Matrix(3,3,6);
+    Matrix* matrix4 = new Matrix(n1, m1, n+1);
 
-    cout << "m1 : " << endl << m1 << endl;
-    cout << "m2 : " << endl << m2 << endl;
-    cout << "m3 : " << endl << m3 << endl;
-    cout << "m4 : " << endl << m4 << endl;
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    cout << "matrix2 : " << endl << matrix2 << endl;
+    cout << "matrix3 : " << endl << matrix3 << endl;
+    cout << "matrix4 : " << endl << matrix4 << endl;
 
     cout << "test constructor copy : " << endl;
-    Matrix c(*m1);
+    Matrix c(*matrix1);
     cout << "c : " << endl << c << endl;
 
     cout << "test operator = : " << endl;
-    Matrix d = *m2;
+    Matrix d = *matrix2;
     cout << "d : " << endl << d << endl;
 
     cout << "test operator : " << endl;
@@ -32,116 +33,132 @@ int main() {
     cout << "ADD with same modulus and size : " << endl;
     cout << "Inline : " << endl;
 
-    m1->add(*m2);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->add(*matrix2);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->addByValue(*m2);
+    d = matrix1->addByValue(*matrix2);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    c = m1->addByPtr(*m2);
+    c = matrix1->addByPtr(*matrix2);
     cout << "d : " << endl << d << endl;
 
     cout << "ADD with same modulus and different size : " << endl;
     cout << "Inline : " << endl;
-    m1->add(*m3);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->add(*matrix3);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->addByValue(*m3);
+    d = matrix1->addByValue(*matrix3);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    d = m1->addByPtr(*m3);
+    d = matrix1->addByPtr(*matrix3);
     cout << "d : " << endl << d << endl;
 
     /*cout << "ADD with different modulus : " << endl;
     cout << "Inline : " << endl;
-    m1->add(*m4);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->add(*matrix4);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->addByValue(*m4);
+    d = matrix1->addByValue(*matrix4);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    d = m1->addByPtr(*m4);
+    d = matrix1->addByPtr(*matrix4);
     cout << "d : " << endl << d << endl;*/
 
     cout << "SUB with same modulus and size : " << endl;
     cout << "Inline : " << endl;
-    m1->sub(*m2);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->sub(*matrix2);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->subByValue(*m2);
+    d = matrix1->subByValue(*matrix2);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    c = m1->subByPtr(*m2);
+    c = matrix1->subByPtr(*matrix2);
     cout << "d : " << endl << d << endl;
 
     cout << "SUB with same modulus and different size : " << endl;
     cout << "Inline : " << endl;
-    m1->sub(*m3);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->sub(*matrix3);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->subByValue(*m3);
+    d = matrix1->subByValue(*matrix3);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    d = m1->subByPtr(*m3);
+    d = matrix1->subByPtr(*matrix3);
     cout << "d : " << endl << d << endl;
 
     /*cout << "SUB with different modulus : " << endl;
     cout << "Inline : " << endl;
-    m1->sub(*m4);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->sub(*matrix4);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->subByValue(*m4);
+    d = matrix1->subByValue(*matrix4);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    d = m1->subByPtr(*m4);
+    d = matrix1->subByPtr(*matrix4);
     cout << "d : " << endl << d << endl;*/
 
     cout << "MULTIPLY with same modulus and size : " << endl;
     cout << "Inline : " << endl;
-    m1->mult(*m2);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->mult(*matrix2);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->multByValue(*m2);
+    d = matrix1->multByValue(*matrix2);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    c = m1->multByPtr(*m2);
+    c = matrix1->multByPtr(*matrix2);
     cout << "d : " << endl << d << endl;
 
     cout << "MULTIPLY with same modulus and different size : " << endl;
     cout << "Inline : " << endl;
-    m1->mult(*m3);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->mult(*matrix3);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->multByValue(*m3);
+    d = matrix1->multByValue(*matrix3);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    d = m1->multByPtr(*m3);
+    d = matrix1->multByPtr(*matrix3);
     cout << "d : " << endl << d << endl;
 
     /*cout << "MULTIPLY with different modulus : " << endl;
     cout << "Inline : " << endl;
-    m1->mult(*m4);
-    cout << "m1 : " << endl << m1 << endl;
-    m1 = &c;
+    matrix1->mult(*matrix4);
+    cout << "matrix1 : " << endl << matrix1 << endl;
+    matrix1 = &c;
     cout << "by value : " << endl;
-    d = m1->multByValue(*m4);
+    d = matrix1->multByValue(*matrix4);
     cout << "d : " << endl << d << endl;
     cout << "by pointer : " << endl;
-    c = m1->multByPtr(*m4);
+    c = matrix1->multByPtr(*matrix4);
     cout << "d : " << endl << d << endl;*/
 
-    delete m1;
-    delete m2;
-    delete m3;
-    delete m4;
+    delete matrix1;
+    delete matrix2;
+    delete matrix3;
+    delete matrix4;
+}
 
-    return 0;
+int main(int argc, char* argv[]) {
+
+    if(argc != 6){
+        std::cerr << "Usage: " << argv[0] << " N1 M1 N2 M2 N" << std::endl;
+        return 1;
+    }
+
+    std::istringstream issn1(argv[1]), issm1(argv[2]), issn2(argv[3]), issm2(argv[4]), issn(argv[5]);
+    unsigned n1, m1, n2, m2, n;
+
+    if(issn1 >> n1 and issm1 >> m1 and issn2 >> n2 and issm2 >> m2 and issn >> n) {
+        testMatrix(n1, m1, n2, m2, n);
+        return 0;
+    }
+    std::cerr << "Error: " << argv[0] << " Arguments are not integers" << std::endl;
+    return 1;
 }
